da26de80178155c978be28e7e71b4c8b4ff14d41
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2025-09-08 22:07:45 +0100

    board and turn display working

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function createGame() {
             return {name, token, getOccupiedSquares, addOccupiedSquare};
         }
 
-        const players = [createPlayer("Player 1", "X"), createPlayer("Player 2", "0")];
+        const players = [createPlayer("Player 1", "x"), createPlayer("Player 2", "o")];
 
         const getPlayers = () => players;
 
@@ -46,6 +46,9 @@ function createGame() {
     })();
 
     const gameController = (function () {
+        let gameState = "playing";
+        const getGameState = () => gameState;
+
         let currentPlayer = playerController.getPlayers()[0];
         const getCurrentPlayer = () => currentPlayer;
         const changeCurrentPlayer = () => {
@@ -61,23 +64,16 @@ function createGame() {
         const increaseTurnCounter = () => turnCounter++;
 
         const checkForTie = () => {
-            if (getTurnCounter() === 9) {
-                return true;
-            } else {
-                return false;
-            }
-        };
+            return (getTurnCounter() === 9);
+        }
 
         const winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
         const checkForWin = () => {
-            const playerSquares = currentPlayer.getOccupiedSquares();
+            const playerSquares = currentPlayer.getOccupiedSquares().map(square => parseInt(square));
 
-            winCombinations.forEach((combination) => {
-                if (combination.every((square) => playerSquares.includes(square))) {
-                    return true;
-                }
-            });
-            return false;
+            return winCombinations.some(combination =>
+                combination.every(square => playerSquares.includes(square))
+            );
         };
 
         const playRound = (squareIndex) => {
@@ -86,21 +82,90 @@ function createGame() {
 
             if (checkForWin()) {
                 console.log(`${currentPlayer.name} won!`);
+                gameState = "win";
             } else if (checkForTie()) {
                 console.log("game is tied");
+                gameState = "tie";
             }
             
             changeCurrentPlayer();
         }
 
-        return {getCurrentPlayer, getTurnCounter, playRound};
+        return {getGameState, getCurrentPlayer, getTurnCounter, playRound};
     })();
 
     return {
         getBoard: gameboard.getSquares,
         getPlayers: playerController.getPlayers,
+        getGameState: gameController.getGameState,
         getCurrentPlayer: gameController.getCurrentPlayer,
         getTurnCounter: gameController.getTurnCounter,
         playRound: gameController.playRound
     }
 }
+
+const screenController = (function () {
+    const game = createGame();
+    const board = document.querySelector("#board");
+    const turnDisplay = document.querySelector("#turn-display");
+    const changeNamesButton = document.querySelector("#name-changer");
+    const clearBoardButton = document.querySelector("#clear-board");
+
+    const createSquareElement = (token, index) => {
+        const square = document.createElement("div");
+        square.classList.add("square", "flex", "items-center");
+        square.dataset.index = index;
+        
+        if (token === "x") {
+            square.classList.add("cross");
+            square.textContent = "x";
+        } else if (token === "o") {
+            square.classList.add("circle");
+            square.textContent = "o";
+        }
+
+        return square;
+    }
+
+    const updateScreen = () => {
+        // update board
+        board.textContent = "";
+
+        const boardSquares = game.getBoard();
+
+        boardSquares.forEach((square, index) => {
+            board.appendChild(createSquareElement(square.getState(), index));
+        });
+
+        // update turn display
+        const currentPlayer = game.getCurrentPlayer();
+        const gameState = game.getGameState();
+
+        if (gameState === "playing") {
+            turnDisplay.textContent = `${currentPlayer.name}'s turn to play`;
+        } else if (gameState === "win") {
+            turnDisplay.textContent = `${currentPlayer.name} won the game`;
+        } else {
+            turnDisplay.textContent = "It's a tie";
+        }
+    }
+
+    // event listeners
+    board.addEventListener("click", e => {
+        if (game.getGameState() !== "playing") return;
+
+        const squareIndex = e.target.dataset.index;
+        game.playRound(squareIndex);
+        updateScreen();
+    });
+
+    changeNamesButton.addEventListener("click", e => {
+        // TODO
+    });
+
+    clearBoardButton.addEventListener("click", e => {
+        // TODO
+    });
+
+    updateScreen();
+})();
